296739da9882c0b4edac8aa6bb96179dd045bfe4
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-05-30 12:35:14 +0800

    develop API for update photo for logged user

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { MulterExtendedModule } from 'nestjs-multer-extended';
 
 import {
   AuthController,
@@ -14,6 +16,20 @@ import { isAuth } from './middleware/is-auth';
 @Module({
   imports: [
     TypeOrmModule.forFeature([Users]),
+    MulterExtendedModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        awsConfig: {
+          credentials: {
+            accessKeyId: config.get<string>('AWS_S3_ACESS_KEY'),
+            secretAccessKey: config.get<string>('AWS_S3_SECRET_ACCESS_KEY'),
+          },
+          region: config.get<string>('AWS_S3_BUCKET_REGION'),
+        },
+        bucket: config.get<string>('AWS_S3_BUCKET_NAME'),
+        basePath: 'pitect-user-avatar',
+      }),
+    }),
     JwtModule.register({
       secret: 'this-is-ultimate-secret-Text!',
       signOptions: {
