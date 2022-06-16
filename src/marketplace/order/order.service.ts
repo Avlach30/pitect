9a06a23648d7b81b6245a5883f07906df4be1bef
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-16 15:21:02 +0800

    develop API feature for upload service result by seller from marketplace orders

--- src/marketplace/order/order.service.ts
+++ src/marketplace/order/order.service.ts
@@ -395,6 +395,60 @@ export class OrderService {
     return objResult;
   }
 
+  async uploadResult(req: any, file: any, orderId: string) {
+    let getOrder;
+    let getCreator;
+
+    if (!file) {
+      throw new BadRequestException(
+        'Please, you must upload a file result of your services',
+      );
+    }
+
+    //* Get order id
+    await this.orderRepository
+      .query('SELECT id, status FROM orders WHERE id = ?', [parseInt(orderId)])
+      .then((data) => {
+        return (getOrder = data[0]);
+      });
+
+    if (getOrder === undefined) {
+      throw new NotFoundException('Data not found');
+    }
+
+    if (getOrder.status != 'Pesanan aktif') {
+      throw new BadRequestException(
+        'Please, upload an order result which is already activated',
+      );
+    }
+
+    await this.orderItemRepository
+      .query(
+        'SELECT orderitems.id, orderitems.orderId, services.creator AS seller FROM orderitems INNER JOIN services ON orderitems.serviceId = services.id WHERE orderitems.orderId = ?',
+        [parseInt(getOrder.id)],
+      )
+      .then((data) => (getCreator = data[0]));
+
+    if (getCreator.seller != parseInt(req.user.userId)) {
+      throw new ForbiddenException('Forbidden to access');
+    }
+
+    const serviceResult = file.Location;
+
+    const currentDate = new Date().toISOString();
+
+    await this.orderRepository.query(
+      'UPDATE orders SET status = "Selesai", doneDate = ?, serviceResult = ? WHERE id = ?',
+      [currentDate, serviceResult, parseInt(getOrder.id)],
+    );
+
+    const objResult = {
+      message: 'Uploading order result by seller successfully',
+      result: serviceResult,
+    };
+    return objResult;
+  }
+
   async createReview(
     orderId: string,
     serviceId: string,
