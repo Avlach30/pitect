f4db2d19674d5a22bf431077cdd3d6f6797fef13
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-22 13:50:49 +0800

    develop API for create a new documentation of photo data for project management

--- src/project/galleries/project-gallery.controller.ts
+++ src/project/galleries/project-gallery.controller.ts
@@ -19,4 +19,31 @@ import { ProjectGalleryService } from './project-gallery.service';
 @Controller('api/projects/:projectId/galleries')
 export class ProjectGalleryController {
   constructor(private projectGalleryService: ProjectGalleryService) {}
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post()
+  @HttpCode(201)
+  @UseInterceptors(
+    AmazonS3FileInterceptor('image', {
+      limits: { fileSize: 1 * 1024 * 1024 },
+      randomFilename: true,
+    }),
+  )
+  async createProjectGallery(
+    @Param('projectId') projectId: string,
+    @Request() req: any,
+    @UploadedFile() file: any,
+    @Body('description') description: string,
+    @Body('date') date: string,
+  ) {
+    const createProjectGallery =
+      await this.projectGalleryService.createProjectGallery(
+        projectId,
+        req,
+        file,
+        description,
+        date,
+      );
+    return createProjectGallery;
+  }
 }
--- src/project/galleries/project-gallery.service.ts
+++ src/project/galleries/project-gallery.service.ts
@@ -8,11 +8,74 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { ProjectGalleries } from '../../entity/project-gallery.entity';
+import { Projects } from '../../entity/project.entity';
+import { ProjectMembers } from '../../entity/project-member.entity';
 
 @Injectable()
 export class ProjectGalleryService {
   constructor(
     @InjectRepository(ProjectGalleries)
-    private serviceRepository: Repository<ProjectGalleries>,
+    private projectGalleryRepository: Repository<ProjectGalleries>,
+    @InjectRepository(Projects)
+    private projectRepository: Repository<Projects>,
+    @InjectRepository(ProjectMembers)
+    private projectMemberRepository: Repository<ProjectMembers>,
   ) {}
+
+  async createProjectGallery(
+    projectId: string,
+    req: any,
+    file: any,
+    description: string,
+    date: string,
+  ) {
+    let project;
+    let member;
+
+    if (!file || !description || !date) {
+      throw new BadRequestException(
+        'Please, input all fields include upload an image file',
+      );
+    }
+
+    await this.projectRepository
+      .query('SELECT id FROM projects WHERE id = ?', [parseInt(projectId)])
+      .then((data) => (project = data[0]));
+
+    if (project == undefined) {
+      throw new NotFoundException('Data not found');
+    }
+
+    await this.projectMemberRepository
+      .query(
+        'SELECT id, user FROM projectmembers WHERE project = ? AND user = ?',
+        [parseInt(projectId), parseInt(req.user.userId)],
+      )
+      .then((data) => (member = data[0]));
+
+    if (member == undefined)
+      throw new ForbiddenException('Forbidden to access');
+
+    const imageUrl = file.Location;
+
+    const formattedDate = new Date(Date.parse(date));
+
+    await this.projectGalleryRepository.query(
+      'INSERT INTO projectgalleries (description, date, imageUrl, userId, projectId) VALUES (?, ?, ?, ?, ?)',
+      [
+        description,
+        formattedDate,
+        imageUrl,
+        parseInt(req.user.userId),
+        parseInt(projectId),
+      ],
+    );
+
+    const objResult = {
+      message: 'Create new documentation for construction project successfully',
+      image: imageUrl,
+    };
+
+    return objResult;
+  }
 }
