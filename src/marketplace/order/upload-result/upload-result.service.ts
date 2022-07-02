f47876892b2d8ab63cddf8817146bd4ecf5366fe
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-07-02 16:10:10 +0800

    hotfixing for order management by seller

--- src/marketplace/order/upload-result/upload-result.service.ts
+++ src/marketplace/order/upload-result/upload-result.service.ts
@@ -58,7 +58,12 @@ export class UploadResultService {
 
     const serviceResult = file.Location;
 
-    const currentDate = new Date().toISOString();
+    const date = new Date().toISOString().split('T')[0];
+    const time = new Date().toISOString().split('T')[1];
+    const hour = time.split(':')[0];
+    const minute = time.split(':')[1];
+    const second = time.split(':')[2].split('.')[0];
+    const currentDate = `${date} ${hour}:${minute}:${second}`;
 
     await this.orderRepository.query(
       'UPDATE orders SET status = "Selesai", doneDate = ?, serviceResult = ? WHERE id = ?',
